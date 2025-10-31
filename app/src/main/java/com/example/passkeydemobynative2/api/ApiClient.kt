123c0e674c20c7ac2ecff21ed5324c4916b79e6b
庞卓健 <pangzhuojian@addcn.com>
2025-10-31 19:09:50 +0800

    添加 asset_statements

--- app/src/main/java/com/example/passkeydemobynative2/api/ApiClient.kt
+++ app/src/main/java/com/example/passkeydemobynative2/api/ApiClient.kt
@@ -9,8 +9,9 @@ import java.util.concurrent.TimeUnit
 object ApiClient {
     
     // 修改为你的服务器地址
-    //private const val BASE_URL = "http://10.0.2.2:8080/api/"
-    private const val BASE_URL = "http://192.168.2.191:8080/"
+    //private const val BASE_URL = "http://10.0.2.2:8080/"
+    //private const val BASE_URL = "http://localhost:8080/"
+    private const val BASE_URL = "https://nonresilient-boundedly-aleta.ngrok-free.dev/"
     // 注意: 10.0.2.2 是 Android 模拟器访问主机 localhost 的特殊 IP
     // 如果使用真机，请使用实际的服务器 IP 地址
     
